4bb1d1db91594fc4e4b1233ed206971f2e836796
thamer <thamer93work@gmail.com>
2024-04-25 23:54:22 +0300

    dolma

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,7 +4,7 @@ const Header = ()=>{
     return(
         <div className="bg-black flex justify-between items-center text-gray-400 px-2 py-4 font-bold fixed w-full top-0 right-0 z-20">
         <h1>My Topics</h1>
-        <Image src='/amina.jpg'
+        <Image src='/dolma.jpg'
         width={64}
         height={64}
         alt="Profile"
