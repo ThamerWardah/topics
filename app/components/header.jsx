b1a30c39e7628ed88c01f080fdf07b110a096623
thamer <thamer93work@gmail.com>
2024-04-25 15:00:19 +0300

    night mode

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const Header = ()=>{
    
     return(
-        <div className="bg-white flex justify-between items-center text-gray-500 px-2 py-4 font-bold fixed w-full top-0 right-0 z-20">
+        <div className="bg-black flex justify-between items-center text-gray-400 px-2 py-4 font-bold fixed w-full top-0 right-0 z-20">
         <h1>My Topics</h1>
         <Image src='/amina.jpg'
         width={64}
--- app/page.js
+++ app/page.js
@@ -17,21 +17,21 @@ export default function Home() {
   }
 
   return (
-    <main className="mt-24 px-2 relative">
+    <main className=" bg-black mt-24 px-2 relative text-white">
       <div>
         <h1 className="text-lg font-bold mb-2">{todayTopics[0].topic}</h1>
 
         <div className="flex flex-col gap-4 px-4">
-          <h2 className="font-bold text-lg text-red-400 border w-fit px-4 rounded bg-slate-100 sticky top-24">
+          <h2 className="font-bold text-lg text-red-400 border w-fit px-4 rounded bg-slate-600 sticky top-24">
             1- Harmful Aspects
           </h2>
           {todayTopics[0].arguments.harmful_aspects.map((item, index) => (
             <div
               key={index}
-              className="bg-gray-100 py-2 px-1"
+              className="bg-gray-300/20 py-2 px-1 rounded-md"
             >
               <h2 className="font-bold text-sm">{item.point}</h2>
-              <h3 className="px-2 text-justify text-xs font-semibold text-gray-600">
+              <h3 className="px-2 text-justify text-xs font-semibold text-gray-300">
                 {item.description}
               </h3>
               <button
@@ -39,7 +39,7 @@ export default function Home() {
                   setIsReading((pre) => !pre)
                   handleClickOnText(item.description, 'US')
                 }}
-                className="bg-green-200 rounded px-2 text-xs font-bold cursor-pointer"
+                className="bg-green-200 rounded text-gray-700 px-2 text-xs font-bold cursor-pointer"
               >
                 {isReading ? 'Stop' : 'Read US'}
               </button>
@@ -49,7 +49,7 @@ export default function Home() {
                   setIsReading((pre) => !pre)
                   handleClickOnText(item.description, 'UK')
                 }}
-                className="bg-blue-300 rounded px-2 text-xs font-bold cursor-pointer mx-2"
+                className="bg-blue-300 text-gray-700 rounded px-2 text-xs font-bold cursor-pointer mx-2"
               >
                 {isReading ? 'Stop' : 'read UK'}
               </button>
@@ -58,16 +58,16 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-4 px-4">
-          <h2 className="font-bold text-green-400 mt-8 border rounded w-fit px-4 py-1 bg-slate-100 sticky top-24">
+          <h2 className="font-bold text-green-400 mt-8 border rounded w-fit px-4 py-1 bg-slate-600 sticky top-24">
             2- Positive Aspects
           </h2>
           {todayTopics[0].arguments.positive_aspects.map((item, index) => (
             <div
               key={index}
-              className="bg-gray-100 py-2 px-1"
+              className="bg-gray-300/20 py-2 px-1 rounded"
             >
               <h2 className=" font-bold text-sm">{item.point}</h2>
-              <h3 className="px-2 text-justify text-xs font-semibold text-gray-600">
+              <h3 className="px-2 text-justify text-xs font-semibold text-gray-300">
                 {item.description}
               </h3>
               <button
@@ -75,7 +75,7 @@ export default function Home() {
                   setIsReading((pre) => !pre)
                   handleClickOnText(item.description, 'US')
                 }}
-                className="bg-green-200 rounded px-2 text-xs font-bold cursor-pointer"
+                className="bg-green-200 text-gray-700 rounded px-2 text-xs font-bold cursor-pointer"
               >
                 {isReading ? 'Stop' : 'Read US'}
               </button>
@@ -85,9 +85,9 @@ export default function Home() {
                   setIsReading((pre) => !pre)
                   handleClickOnText(item.description, 'UK')
                 }}
-                className="bg-blue-300 rounded px-2 text-xs font-bold cursor-pointer mx-2"
+                className="bg-blue-300 text-gray-700 rounded px-2 text-xs font-bold cursor-pointer mx-2"
               >
-                {isReading ? 'Stop' : 'read UK'}
+                {isReading ? 'Stop' : 'Read UK'}
               </button>
             </div>
           ))}
