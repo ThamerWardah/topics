6eacb4d4c4f12f1b986be45c0e59790fffc51d8a
thamer <thamer93work@gmail.com>
2024-04-25 10:32:04 +0300

    second commit

---
+++ app/components/header.jsx
@@ -0,0 +1,11 @@
+
+const Header = ()=>{
+   
+    return(
+        <div className="bg-slate-200 text-gray-500 px-2 py-4 font-bold fixed w-full top-0 right-0">
+        <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header ;
---
+++ app/data/data.js
@@ -0,0 +1,63 @@
+const topics = [
+  {
+    topic: 'Is social media doing more harm than good to society?',
+    arguments: {
+      harmful_aspects: [
+        {
+          point: 'Impact on Mental Health',
+          description:
+            "Numerous studies have linked social media use to increased rates of depression, anxiety, and loneliness, particularly among young people. The constant exposure to curated images and highlight reels of others' lives can lead to feelings of inadequacy and low self-esteem.",
+        },
+        {
+          point: 'Spread of Misinformation',
+          description:
+            'Social media platforms have become breeding grounds for the spread of misinformation and fake news. False information can quickly go viral, leading to widespread belief in falsehoods and undermining trust in credible sources of information.',
+        },
+        {
+          point: 'Cyberbullying',
+          description:
+            'Social media provides a platform for cyberbullying, harassment, and online abuse. The anonymity and distance afforded by the internet can embolden individuals to engage in harmful behavior, leading to serious psychological consequences for victims.',
+        },
+        {
+          point: 'Addiction and Time Wasting',
+          description:
+            'Excessive use of social media can lead to addiction-like behaviors and contribute to decreased productivity. People may find themselves spending countless hours scrolling through feeds, detracting from real-life interactions and activities.',
+        },
+        {
+          point: 'Privacy Concerns',
+          description:
+            "Social media platforms often collect vast amounts of personal data from users, raising concerns about privacy and data security. Instances of data breaches and misuse of personal information have heightened scrutiny of social media companies' practices.",
+        },
+      ],
+      positive_aspects: [
+        {
+          point: 'Connection and Community Building',
+          description:
+            'Social media enables people to connect with others around the world, fostering friendships, support networks, and communities based on shared interests and experiences.',
+        },
+        {
+          point: 'Access to Information and Resources',
+          description:
+            'Social media platforms serve as valuable sources of information, providing news updates, educational content, and resources on various topics. They can also facilitate learning and skill development through online courses and tutorials.',
+        },
+        {
+          point: 'Promotion of Social Causes',
+          description:
+            'Social media has been instrumental in raising awareness about social issues and mobilizing support for causes such as activism, environmental conservation, and humanitarian aid efforts.',
+        },
+        {
+          point: 'Business and Economic Opportunities',
+          description:
+            'Social media platforms offer opportunities for businesses and entrepreneurs to reach a global audience, market their products and services, and engage with customers directly. Influencer marketing has emerged as a lucrative industry, providing income opportunities for content creators.',
+        },
+        {
+          point: 'Platform for Expression and Creativity',
+          description:
+            'Social media allows individuals to express themselves creatively through posts, photos, videos, and artwork. It serves as a platform for sharing ideas, creativity, and cultural expression.',
+        },
+      ],
+    },
+  },
+]
+
+export { topics }
