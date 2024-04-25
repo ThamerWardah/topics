00b374be1ab5b5b35e96b15e91372fbb2345ecc2
thamer <thamer93work@gmail.com>
2024-04-25 13:56:18 +0300

    amina pic has been added

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,9 +1,14 @@
-
+import Image from "next/image";
 const Header = ()=>{
    
     return(
-        <div className="bg-slate-200 text-gray-500 px-2 py-4 font-bold fixed w-full top-0 right-0">
-        <h1>Header</h1>
+        <div className="bg-white flex justify-between items-center text-gray-500 px-10 py-4 font-bold fixed w-full top-0 right-0 z-20">
+        <h1>My Topics</h1>
+        <Image src='/amina.jpg'
+        width={64}
+        height={64}
+        alt="Profile"
+        className="rounded-full" />
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -17,16 +17,19 @@ export default function Home() {
   }
 
   return (
-    <main className="mt-16 px-2">
+    <main className="mt-16 px-2 relative">
       <div>
         <h1 className="text-lg font-bold mb-2">{todayTopics[0].topic}</h1>
 
-        <h2 className="font-bold text-lg text-red-400 border w-fit px-4 rounded bg-slate-100">
-          1- Harmful Aspects
-        </h2>
         <div className="flex flex-col gap-4 px-4">
+          <h2 className="font-bold text-lg text-red-400 border w-fit px-4 rounded bg-slate-100 sticky top-24">
+            1- Harmful Aspects
+          </h2>
           {todayTopics[0].arguments.harmful_aspects.map((item, index) => (
-            <div key={index}>
+            <div
+              key={index}
+              className="bg-gray-100 py-2 px-1"
+            >
               <h2 className="font-bold">{item.point}</h2>
               <h3 className="px-2 text-justify text-sm font-semibold text-gray-600">
                 {item.description}
@@ -54,14 +57,17 @@ export default function Home() {
           ))}
         </div>
 
-        <h2 className="font-bold text-lg text-green-400 mt-8">
-          2- Positive Aspects
-        </h2>
         <div className="flex flex-col gap-4 px-4">
+          <h2 className="font-bold text-green-400 mt-8 border rounded w-fit px-4 py-1 bg-slate-100 sticky top-24">
+            2- Positive Aspects
+          </h2>
           {todayTopics[0].arguments.positive_aspects.map((item, index) => (
-            <div key={index}>
+            <div
+              key={index}
+              className="bg-gray-100 py-2 px-1"
+            >
               <h2 className=" font-bold">{item.point}</h2>
-              <h3 className="px-2 text-justify font-semibold text-gray-600">
+              <h3 className="px-2 text-justify text-xs font-semibold text-gray-600">
                 {item.description}
               </h3>
             </div>
